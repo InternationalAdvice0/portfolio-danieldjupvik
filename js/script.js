deb98b67f9cb5360efcbf292282fba51607c80b9
Daniel Djupvik Sætre <danieldjupvik@Daniels-MBP.lan>
2020-04-30 06:02:28 +0200

    Added final design elements and some javascript

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
 /*----- Hamburger menu -----*/
-function changeClassAndAnimateButton(x) {
-  x.classList.toggle("change");
-  var x = document.getElementById("topnav-container");
+function changeClassAndAnimateButton(elem) {
+  elem.classList.toggle("change"); // could also used addEventListener instead and select the element
+  var topnavContainer = document.getElementById("topnav-container");
   var topnav = document.querySelector(".topnav");
   var footer = document.querySelector(".footer");
   var backgroundElem = document.querySelector(".main-background");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+
+  if (topnavContainer.style.display === "block") {
+    topnavContainer.style.display = "none";
     topnav.classList.remove("backgroundColorTopnav");
     backgroundElem.classList.remove("addBlur");
     footer.classList.remove("addBlur");
   } else {
-    x.style.display = "block";
+    topnavContainer.style.display = "block";
     topnav.classList.add("backgroundColorTopnav");
     backgroundElem.classList.add("addBlur");
     footer.classList.add("addBlur");
   }
 }
 
-/* Fix a bug when you opened the hamburger menu and closed it on mobile, it wouldn't show on desktop */
-var x = document.getElementById("topnav-container");
+/* Fix a bug when you opened the hamburger menu on mobile and then resize window to desktop, topnav wouldn't show on desktop */
+var elem = document.getElementById("topnav-container");
+var topnav = document.querySelector(".topnav");
+var mainBackground = document.querySelector(".main-background");
 window.addEventListener("resize", displayWindowSize);
 
 function displayWindowSize() {
   var currentWidth = window.innerWidth;
 
-  if (currentWidth > 630) {
-    x.style.display = "block";
+  if (currentWidth > 672) {
+    elem.style.display = "block";
   } else {
-    x.style.display = "none";
+    elem.style.display = "none";
+    topnav.classList.remove("backgroundColorTopnav");
+    mainBackground.classList.remove("addBlur");
   }
 }
+
+/* Update year in footer */
+var copyrightElem = document.getElementById("copyright-element");
+
+var date = new Date();
+var currentYear = date.getFullYear();
+var string = copyrightElem.innerHTML;
+var replacedString = string.replace("year", currentYear);
+
+copyrightElem.innerHTML = replacedString;
